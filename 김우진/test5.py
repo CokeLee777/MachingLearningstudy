55fdfe28f7515d70b2a0343b9c64b4439b7aefe4
woojin1027 <woojinny@kakao.com>
2020-10-07 22:14:33 +0900

    10.07_러닝

---
+++ 김우진/test5.py
@@ -0,0 +1,40 @@
+# tensorflow와 tf.keras를 임포트합니다
+import tensorflow as tf
+from tensorflow import keras
+
+# 헬퍼(helper) 라이브러리를 임포트합니다
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+train_images.shape
+len(train_labels)
+train_labels
+test_images.shape
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+img = test_images[0]
+
+print(img.shape)
